8b60d15c237bca38bf99fb049608d5d6afcd9434
nils <nils.telleria@datys.cu>
2015-11-19 22:28:33 -0500

    (+) Added main initial code.

---
+++ SPGenericListRSS/SPGenericListRSS/Layouts/SPGenericListRSS/rss.aspx
@@ -0,0 +1,47 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Import Namespace="Microsoft.SharePoint.ApplicationPages" %>
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="rss.aspx.cs" Inherits="SPGenericListRSS.Layouts.SPGenericListRSS.rss"%>
+<rss version="2.0">
+  <channel>
+
+    <title><asp:Literal runat="server" ID="litRssTitle"></asp:Literal></title>
+
+    <link><asp:Literal runat="server" ID="litRssLink"></asp:Literal></link>  
+    <description><asp:Literal runat="server" ID="litRssDescription"></asp:Literal></description>  
+                    
+    <lastBuildDate><asp:Literal runat="server" ID="litRssLastBuildDate"></asp:Literal></lastBuildDate>
+    <generator>generator</generator>
+    <ttl><asp:Literal runat="server" ID="litRssTtl"></asp:Literal></ttl>
+    <language><asp:Literal runat="server" ID="litRssLanguage"></asp:Literal></language>
+    <image>
+        <title><asp:Literal runat="server" ID="litRssImageTitle"></asp:Literal></title>
+        <url><asp:Literal runat="server" ID="litRssImageUrl"></asp:Literal></url>
+        <link><asp:Literal runat="server" ID="litRssImageLink"></asp:Literal></link>
+    </image>    
+
+<asp:Repeater ID="RepeaterRSS" runat="server">  
+        <HeaderTemplate>  
+
+        </HeaderTemplate>  
+        <ItemTemplate>  
+            <item>  
+                <title><%# RemoveIllegalCharacters(DataBinder.Eval(Container.DataItem, "Title")) %></title>  
+                <link><%# DataBinder.Eval(Container.DataItem, "Link") %></link>  
+                <author><%# RemoveIllegalCharacters(DataBinder.Eval(Container.DataItem, "Author"))%></author>  
+                <pubDate><%# String.Format("{0:R}", DataBinder.Eval(Container.DataItem, "PubDate"))%></pubDate>  
+                <description><%# RemoveIllegalCharacters(DataBinder.Eval(Container.DataItem, "Description"))%></description>  
+                <guid isPermaLink="true"><%# DataBinder.Eval(Container.DataItem, "PermalinkGuid") %></guid>
+        </item>  
+        </ItemTemplate>  
+        <FooterTemplate>  
+                
+        </FooterTemplate>  
+</asp:Repeater>  
+      </channel>  
+</rss>  
---
+++ SPGenericListRSS/SPGenericListRSS/Layouts/SPGenericListRSS/rss.aspx.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.WebControls;
+
+namespace SPGenericListRSS.Layouts.SPGenericListRSS
+{
+    public class RssItem
+    {
+        public String PermalinkGuid { get; set; }
+        public String Title { get; set; }
+        public String Link { get; set; }
+        public String Description { get; set; }
+        public DateTime PubDate { get; set; }
+        public String Author { get; set; }
+
+
+    }
+
+    public class RssFeedDto
+    {
+        public RssFeedDto()
+        {
+            Items = new List<RssItem>();
+        }
+        public DateTime LastBuildDate { get; set; }
+        public string Description { get; set; }
+        public String ImageUrl { get; set; }
+        public String ImageLink { get; set; }
+        public String ImageTitle { get; set; }
+        public string Language { get; set; }
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public String XsltLink { get; set; }
+        public int Ttl { get; set; }
+        public List<RssItem> Items { get; set; }
+
+        public String ETag { get; set; }
+
+
+
+
+    }
+
+
+    public partial class rss : LayoutsPageBase
+    {
+        private void BindData(RssFeedDto feed)
+        {
+            litRssLastBuildDate.Text = feed.LastBuildDate.ToString();
+            litRssDescription.Text = feed.Description;
+            litRssImageUrl.Text = feed.ImageUrl;
+            litRssImageLink.Text = feed.ImageLink;
+            litRssImageTitle.Text = feed.ImageTitle;
+            litRssLanguage.Text = feed.Language;
+            litRssLink.Text = feed.Link;
+            litRssTitle.Text = feed.Title;
+            litRssTtl.Text = feed.Ttl.ToString();
+            RepeaterRSS.DataSource = feed.Items;
+            RepeaterRSS.DataBind();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get previous page title and URL
+                SPSecurity.RunWithElevatedPrivileges(delegate()
+                {
+                    var idStr = HttpContext.Current.Request.QueryString["List"];
+                    var idViewStr = HttpContext.Current.Request.QueryString["View"];
+                    var fieldTitle = HttpContext.Current.Request.QueryString["T"];
+                    var fieldAuthor = HttpContext.Current.Request.QueryString["A"];
+                    var fieldDate = HttpContext.Current.Request.QueryString["D"];
+                    var fieldBody = HttpContext.Current.Request.QueryString["B"];
+                    var RelURL = HttpContext.Current.Request.QueryString["U"];
+                    var web = SPContext.Current.Web;
+                    var feed = new RssFeedDto();
+                    if (PopulateFromList(feed, web, idStr, idViewStr, fieldTitle, fieldAuthor, fieldDate, fieldBody, RelURL))
+                    {
+                        BindData(feed);
+                        Response.AppendHeader("ETag", feed.ETag);
+                        Response.AppendHeader("Last-Modified", feed.LastBuildDate.ToString());
+                        Response.ContentType = "text/xml";
+                        Response.ContentEncoding = Encoding.UTF8;
+                    }
+
+                });
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public string GetProperty(object val, string defaultVal)
+        {
+            if (val == null || String.IsNullOrEmpty(val.ToString())) return defaultVal;
+            return val.ToString();
+        }
+
+        private bool PopulateFromList(RssFeedDto feed, SPWeb web, string listIdStr, string viewIdStr, string fieldTitle, string fieldAuthor, string fieldDate, string fieldBody, string RelURL)
+        {
+
+            try
+            {
+                var list = web.Lists[listIdStr];
+                var limit = int.Parse(GetProperty(list.RootFolder.Properties["vti_rss_ItemLimit"], "30"));
+                var allItemsLink = SPUtility.ConcatUrls(web.Url, list.DefaultView.Url);
+                feed.LastBuildDate = list.LastItemModifiedDate;
+                feed.ETag = list.CurrentChangeToken.ToString();
+                feed.Description = GetProperty(list.RootFolder.Properties["vti_rss_ChannelDescription"], list.Description);
+                feed.ImageUrl = ConvertRelativeUrlToAbsoluteUrl(GetProperty(list.RootFolder.Properties["vti_rss_ChannelImageUrl"], list.ImageUrl));
+                feed.ImageLink = allItemsLink;
+                feed.ImageTitle = list.Title;
+                feed.Language = web.UICulture.Name;
+                feed.Link = allItemsLink;
+                feed.Title = GetProperty(list.RootFolder.Properties["vti_rss_ChannelTitle"], list.Title);
+                feed.XsltLink = web.Url + "/_layouts/RssXslt.aspx?List=" + list.ID;
+                feed.Ttl = 60;
+
+                for (var i = 0; i < list.Items.Count; i++)
+                {
+                    try
+                    {
+                        var listItem = list.Items[i];
+
+                        var feedItem = new RssItem();
+                        var itemUrl = web.Site.RootWeb.Url + RelURL + "?ID=" + listItem.ID;
+                        feedItem.PermalinkGuid = itemUrl;
+                        feedItem.Title = listItem[fieldTitle].ToString();
+                        feedItem.Title = Regex.Replace(feedItem.Title, @"<[^>]+>|&nbsp;", "").Trim();
+                        feedItem.Link = itemUrl;
+                        feedItem.Description = listItem[fieldBody] != null ? listItem[fieldBody].ToString() : "";
+                        try
+                        {
+                            feedItem.PubDate = Convert.ToDateTime(listItem[fieldDate].ToString());
+                        }
+                        catch
+                        {
+                            try
+                            {
+                                feedItem.PubDate = Convert.ToDateTime(listItem["Modified"].ToString());
+                            }
+                            catch
+                            {
+                                feedItem.PubDate = DateTime.Today;
+                            }
+                        }
+                        feedItem.Author = listItem[fieldAuthor].ToString();
+                        feed.Items.Add(feedItem);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+                feed.Items = feed.Items.OrderByDescending(x => x.PubDate).Take(limit).ToList();
+
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+            }
+            return false;
+        }
+
+        public string ConvertRelativeUrlToAbsoluteUrl(string relativeUrl)
+        {
+            if (Request.IsSecureConnection)
+                return string.Format("https://{0}{1}", Request.Url.Host, Page.ResolveUrl(relativeUrl));
+            else
+                return string.Format("http://{0}{1}", Request.Url.Host, Page.ResolveUrl(relativeUrl));
+        }
+
+        protected string RemoveIllegalCharacters(object input)
+        {
+            // cast the input to a string  
+            string data = input.ToString();
+
+            // replace illegal characters in XML documents with their entity references  
+            data = data.Replace("&", "&amp;");
+            data = data.Replace("\"", "&quot;");
+            data = data.Replace("'", "&apos;");
+            data = data.Replace("<", "&lt;");
+            data = data.Replace(">", "&gt;");
+
+            return data;
+        }
+
+    }
+}
